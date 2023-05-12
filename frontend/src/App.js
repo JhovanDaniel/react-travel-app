da6eb2527cdbba732873a2b5bd37c0eece69d625
Jhovan Daniel <jed031@cs.bham.ac.uk>
2023-05-12 16:58:44 -0400

    Added map front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+// import ReactMapGL from 'react-map-gl';
+import Map, {Marker, Popup} from 'react-map-gl';
+import { Room, Star } from '@material-ui/icons'
+import "./app.css"
+
 
 function App() {
+const [viewport, setViewport] = React.useState({
+  width: "100vw",
+  height: "100vh",
+  latitude: 18.200409624631693,
+  longitude: -63.06103528533779,
+  zoom: 15
+})
+const [showPopup, setShowPopup] = React.useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Map
+      initialViewState={{
+        ...viewport
+      }}
+      style={{width: "100vw", height: "100vh"}}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      mapStyle="mapbox://styles/mapbox/outdoors-v12"
+    >
+      <Marker longitude={-63.06103528533779} latitude={18.200409624631693} offsetLeft={-20} offsetRight={-10}>
+        {/* Figue how to change font size on zoom */}
+        <Room style={{fontSize:viewport.zoom * 2, color:"slateblue"}}/>
+      </Marker>
+      {/* <Popup 
+        longitude={-63.06103528533779} 
+        latitude={18.200409624631693}
+        anchor="left"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className='card'>
+         <label>Place</label>
+         <h4 className='place'> Little Harbor</h4>
+         <label>Review</label>
+         <p className='desc'> Beautiful place. I like it.</p>
+         <label>Rating</label>
+         <div className='stars'>
+          <Star className='star'/>
+          <Star className='star'/>
+          <Star className='star'/>
+          <Star className='star'/>
+          <Star className='star'/>
+         </div>
+         <label>Information</label>
+         <span className='username'> Created by <b> Jhovan</b></span>
+         <span className='date'> 1 hour ago </span>
+        </div>
+      </Popup> */}
+    </Map>
+    
   );
 }
 
