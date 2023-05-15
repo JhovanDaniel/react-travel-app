bbc4bfcde2f3130f0f2c91a1a07435a436d4f2f3
Jhovan Daniel <jed031@cs.bham.ac.uk>
2023-05-15 17:47:48 -0400

    Added marker popup functionality. Not working completely

--- backend/routes/pins.js
+++ backend/routes/pins.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try{
     const pins = await Pin.find()
-    res.status(200).json
+    res.status(200).json(pins)
   }catch(err){
     res.status(500).json(err)
   }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,19 @@ import * as React from 'react';
 // import ReactMapGL from 'react-map-gl';
 import Map, {Marker, Popup} from 'react-map-gl';
 import { Room, Star } from '@material-ui/icons'
+import axios from 'axios'
+import {format} from 'timeago.js'
 import "./app.css"
 
 
 function App() {
+
+const [pins, setPins] = React.useState([])
+
+const [currentPlaceId, setCurrentPlaceId] = React.useState(null)
+
+const [showPopup, setShowPopup] = React.useState(true);
+
 const [viewport, setViewport] = React.useState({
   width: "100vw",
   height: "100vh",
@@ -13,45 +22,77 @@ const [viewport, setViewport] = React.useState({
   longitude: -63.06103528533779,
   zoom: 15
 })
-const [showPopup, setShowPopup] = React.useState(true);
+
+React.useEffect(() => {
+  const getPins = async () => {
+    try{ 
+      const res = await axios.get("/pins");
+      setPins(res.data)
+    } catch(err){
+      console.log(err)
+    }
+  };
+  getPins()
+}, [])
+
+const handleMarkerClick = (id) => {
+  setCurrentPlaceId(id)
+  // setShowPopup((prevShowPopup) => !prevShowPopup)
+}
+
+//State check
+
 
   return (
-    <Map
-      initialViewState={{
-        ...viewport
-      }}
-      style={{width: "100vw", height: "100vh"}}
-      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-      mapStyle="mapbox://styles/mapbox/outdoors-v12"
-    >
-      <Marker longitude={-63.06103528533779} latitude={18.200409624631693} offsetLeft={-20} offsetRight={-10}>
-        {/* Figue how to change font size on zoom */}
-        <Room style={{fontSize:viewport.zoom * 2, color:"slateblue"}}/>
-      </Marker>
-      {/* <Popup 
-        longitude={-63.06103528533779} 
-        latitude={18.200409624631693}
-        anchor="left"
-        >
-        <div className='card'>
-         <label>Place</label>
-         <h4 className='place'> Little Harbor</h4>
-         <label>Review</label>
-         <p className='desc'> Beautiful place. I like it.</p>
-         <label>Rating</label>
-         <div className='stars'>
-          <Star className='star'/>
-          <Star className='star'/>
-          <Star className='star'/>
-          <Star className='star'/>
-          <Star className='star'/>
-         </div>
-         <label>Information</label>
-         <span className='username'> Created by <b> Jhovan</b></span>
-         <span className='date'> 1 hour ago </span>
-        </div>
-      </Popup> */}
-    </Map>
+    <div className='App'>
+      <Map
+        initialViewState={{
+          ...viewport
+        }}
+        style={{width: "100vw", height: "100vh"}}
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        mapStyle="mapbox://styles/mapbox/outdoors-v12"
+      >
+        
+        {pins.map(p =>(
+          console.log(currentPlaceId, p._id),
+          <>
+          <Marker longitude={p.long} latitude={p.lat} offsetLeft={-20} offsetRight={-10}>
+
+            <Room style={{fontSize:viewport.zoom * 2, color:"slateblue"}}
+            onClick={()=>handleMarkerClick(p._id)}
+            />
+          </Marker>
+
+          {p._id === currentPlaceId && (
+          <Popup 
+            longitude={p.long} 
+            latitude={p.lat}
+            anchor="left"
+            >
+            <div className='card'>
+            <label>Place</label>
+            <h4 className='place'>{p.title}</h4>
+            <label>Review</label>
+            <p className='desc'>{p.desc}</p>
+            <label>Rating</label>
+            <div className='stars'>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+              <Star className='star'/>
+            </div>
+            <label>Information</label>
+            <span className='username'> Created by <b>{p.username}</b></span>
+            <span className='date'> {format(p.createdAt)} </span>
+            </div>
+          </Popup>)
+          }
+        </>
+        ))}
+      </Map>
+    </div>
     
   );
 }
