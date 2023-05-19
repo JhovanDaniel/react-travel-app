1fb3df90bfbdddb28e047a9e659dd232154c9db8
Jhovan Daniel <jed031@cs.bham.ac.uk>
2023-05-19 16:21:24 -0400

    Modified user and pin routes for production

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -19,7 +19,7 @@ export default function Register({setShowRegister}){
       password:passwordRef.current.value
     };
     try{
-      await axios.post("/users/register", newUser);
+      await axios.post("https://react-travel-app-api.vercel.app/api/users/register", newUser);
       setError(false)
       setSuccess(true)
     } catch(err){
